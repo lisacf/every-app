682928b0425b902a57766e0bb80bf1b62abf3bf2
Lisa Friedman <lisacf@gmail.com>
2013-10-02 14:22:40 -0700

    first commit bootstrap devise and omniauth

---
+++ app/controllers/cupcakes_controller.rb
@@ -0,0 +1,4 @@
+class CupcakesController < ApplicationController
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Everyapp::Application.config.secret_key_base = 'c90fc02e927125c5d3ca1b06ced065611860d403335048f2b0abe13872e61231b8b6498055421264465c0c77e6ae59c0fea39afdc5ee8f2618f35bcd9920dfcb'
